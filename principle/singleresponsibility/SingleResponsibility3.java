dad92de68f28796d4b7ba3f04deabbae6468ea9f
CCH0124 <cchong0124@gmail.com>
2020-02-26 18:41:28 +0800

    single Responsiblity

---
+++ principle/singleresponsibility/SingleResponsibility3.java
@@ -0,0 +1,24 @@
+package principle.singleresponsibility;
+/**
+ * 對類別沒有大修改，只是增加方法
+ * 沒有在 class 級別上遵守單一職責，但在方法級別上，扔然遵守
+ */
+class Vehicle {
+    public void run(String vehicle) {
+        System.out.println(vehicle + "road");
+    }
+    public void runAir(String vehicle) {
+        System.out.println(vehicle + "Sky");
+    }
+    public void runWater(String vehicle) {
+        System.out.println(vehicle + "Water");
+    }
+}
+public class SingleResponsibility3 {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.run("Car");
+        vehicle.runAir("Airplan");
+        vehicle.runWater("Ship");
+    }
+}
