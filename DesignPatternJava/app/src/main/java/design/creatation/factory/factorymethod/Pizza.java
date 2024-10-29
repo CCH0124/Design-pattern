158ce32041d238d410d42e22d42b303d5d244bf1
CCH0124 <cchong0124@gmail.com>
2024-10-29 23:10:29 +0800

    feat: add Factory Method

---
+++ DesignPatternJava/app/src/main/java/design/creatation/factory/factorymethod/Pizza.java
@@ -0,0 +1,37 @@
+package design.creatation.factory.factorymethod;
+
+import java.util.ArrayList;
+
+public abstract class Pizza {
+    String name;
+	String dough;
+	String sauce;
+	ArrayList<String> toppings = new ArrayList<>();
+
+    void prepare() {
+		System.out.printf("Prepare %s \n", name);
+		System.out.println("Tossing dough...");
+		System.out.println("Adding sauce...");
+		System.out.println("Adding toppings: ");
+		toppings.forEach( t -> System.out.println("   " + t));
+	}
+
+    /**
+     * 烘焙
+     */
+    public void bake(){
+        System.out.println(name + " baking...");
+    }
+    /**
+     * 切割
+     */
+    public void cut(){
+        System.out.println(name + " cutting...");
+    }
+    /**
+     * 打包
+     */
+    public void box(){
+        System.out.println(name + " boxing...");
+    }
+}
